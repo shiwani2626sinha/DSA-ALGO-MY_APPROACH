fd24f50ad6e6267716c73e1914b3bee5ba7bf9b2
shiwani sinha <35289975+shiwani2626sinha@users.noreply.github.com>
2021-12-05 20:07:49 +0530

    Update detect_remove_loop_in_linkedlist.java

--- GFG/detect_remove_loop_in_linkedlist.java
+++ GFG/detect_remove_loop_in_linkedlist.java
@@ -30,3 +30,33 @@ class Solution {
 */
 
 // OPTIMISED SOLUTION
+
+class Solution
+{
+    //Function to remove a loop in the linked list.
+    public static void removeLoop(Node head){
+        // code here
+        // remove the loop without losing any nodes
+     
+     Node slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                break;
+            }
+        }
+        if(slow != fast)
+            return;
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        while(fast.next != slow){
+            fast = fast.next;
+        }
+        fast.next = null;
+     
+    }
+}
