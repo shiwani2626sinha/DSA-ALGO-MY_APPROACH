7c877c32e1bca954d30c125bc6a888b9e272af82
shiwani sinha <35289975+shiwani2626sinha@users.noreply.github.com>
2021-12-05 16:11:31 +0530

    Create Detect_loop_linkedlist.java

---
+++ GFG/Detect_loop_linkedlist.java
@@ -0,0 +1,15 @@
+/*Given a linked list of N nodes. The task is to check if the linked list has a loop. Linked list can contain self loop.*/
+class Solution {
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){
+        // Add code here
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && slow!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
